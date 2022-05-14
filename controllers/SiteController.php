b988469cfa409a1a31606c9b6c3071c3a9ac9eee
olgamaksakova <maksakovaolga28@gmail.com>
2022-05-14 12:09:26 +0300

    first commit

---
+++ controllers/SiteController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\helpers\Json;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex($background =  '', $depth = 1)
+    {
+        $data = $this->list('', 2);
+        return $this->render('index', compact('data','depth'));
+    }
+
+    public function list($background =  '', $depth = 1)
+    {
+        $file = dirname(__DIR__, 1) . '/web/file/file.json';
+        $content = file_get_contents($file);
+        $content = Json::decode($content, true);
+
+        if($depth === 1){
+            foreach ($content as $item){
+                unset($item['value']['depth']);
+            }
+        }
+
+        return $content;
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
---
+++ views/site/index.php
@@ -0,0 +1,46 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+
+
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+            <ul id="list">
+                <?php foreach ($data as $item) {?>
+                    <li>
+                        <?= $item['name'] ?>
+                        <ul>
+                            <li>
+                                <?= $item['value']['type'] ?>
+                            </li>
+                            <?php if ($depth > 1) { die;?>
+                                <li>
+                                    <ul>
+                                        <li><?= $item['value']['depth']['value'] ?></li>
+                                    </ul>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </li>
+                    <?php
+                } ?>
+            </ul>
+        </div>
+
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#list > li').click(function (event) {
+            $(this).children("ul").slideToggle();
+            event.stopPropagation();
+        });
+    });
+</script>
